f8390ede5ddb59649d1ef3afd87d759bb410ec79
3veryday <hyunseo.3veryday.ji@gmail.com>
2025-06-27 18:24:47 +0900

    basic project setup

---
+++ Back/src/main/java/_veryday/Sched/SchedApplication.java
@@ -0,0 +1,13 @@
+package _veryday.Sched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedApplication.class, args);
+	}
+
+}
